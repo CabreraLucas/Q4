b1c5feb401756b1101af3256dca3e63dfb56d768
CabreraLucas <lucas.cabrera@fatec.sp.gov.br>
2022-06-06 17:30:09 +0000

    Atividade4

---
+++ Numeros.java
@@ -0,0 +1,8 @@
+public class Numeros {
+    public static void main(String[] args){
+        for (int n=38; n<=1000;n++){
+                System.out.print("-"+n);
+            
+        }
+    }
+}
